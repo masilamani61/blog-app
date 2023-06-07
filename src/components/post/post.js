b271037f406edbbc7ba4267d428c7cb2ffb52385
masilamani61 <118548969+masilamani61@users.noreply.github.com>
2023-06-07 14:46:04 +0530

    Update post.js

--- src/components/post/post.js
+++ src/components/post/post.js
@@ -2,7 +2,7 @@ import "./post.css";
 import { Link } from "react-router-dom";
 
 export default function Post({ post }) {
-  const PF = "https://blog-app-api-lv3i.onrender.com/images/";
+  const PF = "https://blog-app-api1.onrender.com/images/";
   return (
     <div className="post">
       {post.photo && <img className="postImg" src={PF + post.photo} alt="" />}
