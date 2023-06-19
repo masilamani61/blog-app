3fc5ed06ea3abe8716800c5c5d2cb782297e2850
masilamani61 <118548969+masilamani61@users.noreply.github.com>
2023-06-19 11:14:20 +0530

    Add files via upload

--- src/pages/write/write.js
+++ src/pages/write/write.js
@@ -2,6 +2,8 @@ import { useContext, useState } from 'react'
 import axios from 'axios'
 import './write.css'
 import { Context } from '../../context/context'
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { storage } from '../../firebas';
 export default function Write(){
     const [title,settitle]=useState('')
     const [desc,setdesc]=useState('')
@@ -15,29 +17,64 @@ export default function Write(){
             title,
             desc,
         }
-        if (file){ 
-            const data=new FormData();
-            const filename=Date.now()+file.name;;
-            data.append('name',filename)
-            data.append('file',file)
-            newpost.photo=filename;
-            try{
-                await axios.post('https://blog-app-api1.onrender.com/api/upload',data)
-            }
-            catch(err){
+        if (file){
+            const storageRef = ref(storage, title);
 
-            }
+const uploadTask = uploadBytesResumable(storageRef, file);
+
+// Register three observers:
+// 1. 'state_changed' observer, called any time the state changes
+// 2. Error observer, called on failure
+// 3. Completion observer, called on successful completion
+uploadTask.on('state_changed', 
+  (snapshot) => {
+    // Observe state change events such as progress, pause, and resume
+    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    console.log('Upload is ' + progress + '% done');
+    switch (snapshot.state) {
+      case 'paused':
+        console.log('Upload is paused');
+        break;
+      case 'running':
+        console.log('Upload is running');
+        break;
+    }
+  }, 
+  (error) => {
+    // Handle unsuccessful uploads
+  }, 
+  () => {
+    // Handle successful uploads on complete
+    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+    getDownloadURL(uploadTask.snapshot.ref).then(async(downloadURL) => {
+        newpost.photo=downloadURL
+        console.log(newpost)
+        try{
+          const res=await  axios.post('http://localhost:5000/api/post',newpost)
+          window.location.replace('http://localhost:3000/post/'+res.data._id)
+       }
+       catch{
+           
+       }
+      
+    });
+  }
+);
+          
+        }
+        else{
             try{
-               const res= await axios.post('https://blog-app-api1.onrender.com/api/post',newpost)
-               window.location.replace('/post/'+res.data._id)
+               const res= await axios.post('http://localhost:5000/api/post',newpost)
+               window.location.replace('http://localhost:3000/post/'+res.data._id)
             }
             catch{
 
-            }
+            }}
         }
 
         
-    }
+    
 
     return (
         <div className='write'>
@@ -65,4 +102,4 @@ export default function Write(){
             </form>
         </div>
     )
-}
+            }
